76d1eafc618edadffd5e817b96d36d061fc7dbfd
mariannacoppa <densimetra@gmail.com>
2024-09-27 15:25:15 +0200

    [UPD] defined migration structure

--- database/migrations/2024_09_27_131638_create_trains_table.php
+++ database/migrations/2024_09_27_131638_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 100)->nullable();
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code');
+            $table->tinyInteger('train_wagons')->nullable();
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
