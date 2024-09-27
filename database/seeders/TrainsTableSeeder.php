4611508c7957e16d675097cf975cec009217bf31
mariannacoppa <densimetra@gmail.com>
2024-09-27 16:12:40 +0200

    [UPD] set a value with Faker class to all model fields

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -18,6 +18,15 @@ class TrainsTableSeeder extends Seeder
     {
         for($i=0; $i<25; $i++){
             $train = new Train();
+            $train->company = $faker->company();
+            $train->departure_station = $faker->randomElement(['Milano', 'Roma', 'Bologna']);
+            $train->arrival_station = $faker->randomElement(['Torino', 'Napoli', 'Genova']);
+            $train->departure_time = $faker->dateTimeBetween('- 1 day', '+ 1 day');
+            $train->arrival_time = $faker->dateTimeBetween('+ 1 day', '+ 2 day');
+            $train->train_code = 'TR'.$faker->randomNumber(3, true);
+            $train->train_wagons = $faker->randomDigitNotNull();
+            $train->on_time = $faker->boolean();
+            $train->cancelled = $faker->boolean();
         }
     }
 }
